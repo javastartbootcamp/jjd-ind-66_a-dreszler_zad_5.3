50988f8ef46e593b9dc516cbb05fac590a76a864
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-25 11:05:44 +0100

    Poprawa deklaracji i nazewnictwa stałych

--- src/main/java/pl/javastart/task/logic/PriceConverter.java
+++ src/main/java/pl/javastart/task/logic/PriceConverter.java
@@ -3,20 +3,20 @@ package pl.javastart.task.logic;
 import pl.javastart.task.model.Product;
 
 public class PriceConverter {
-    private final double foodTaxRate = 0.1;
-    private final double electronicsTaxRate = 0.2;
-    private final double defaultTaxRate = 0.23;
-    private final String food = "jedzenie";
-    private final String electronics = "elektronika";
+    private static final double FOOD_TAX_RATE = 0.1;
+    private static final double ELECTRONICS_TAX_RATE = 0.2;
+    private static final double DEFAULT_TAX_RATE = 0.23;
+    private static final String FOOD = "jedzenie";
+    private static final String ELECTRONICS = "elektronika";
 
     public double getGrossPrice(Product product) {
         String category = product.getCategory();
         double netPrice = product.getNetPrice();
 
         double grossPrice = switch (category) {
-            case food -> netPrice * (1 + foodTaxRate);
-            case electronics -> netPrice * (1 + electronicsTaxRate);
-            default -> netPrice * (1 + defaultTaxRate);
+            case FOOD -> netPrice * (1 + FOOD_TAX_RATE);
+            case ELECTRONICS -> netPrice * (1 + ELECTRONICS_TAX_RATE);
+            default -> netPrice * (1 + DEFAULT_TAX_RATE);
         };
 
         return Math.round(grossPrice * 100) / 100.0;
